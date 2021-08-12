9b59721eed338f71363d5055fb76527228d95168
Mario Sánchez García <mario@purestake.com>
2021-08-12 18:37:54 +0200

    add: custom 7.0.1 version

--- mitmproxy/version.py
+++ mitmproxy/version.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import sys
 
-VERSION = "7.0.1"
+VERSION = "7.0.1.ps"
 MITMPROXY = "mitmproxy " + VERSION
 
 # Serialization format version. This is displayed nowhere, it just needs to be incremented by one
